8672df845b330df646f5fa3a9a0e4430f8336a76
steffenwoell <56505661+steffenwoell@users.noreply.github.com>
2021-05-28 14:26:20 +0200

    Update lastmod.js

--- js/lastmod.js
+++ js/lastmod.js
@@ -5,9 +5,6 @@ function date_ddmmmyy(date)
   var m = date.getMonth() + 1;
   var y = date.getYear();
 
-  // handle different year values
-  // returned by IE and NS in
-  // the year 2000.
   if(y >= 2000)
   {
     y -= 2000;
@@ -17,9 +14,6 @@ function date_ddmmmyy(date)
     y -= 100;
   }
 
-  // could use splitString() here
-  // but the following method is
-  // more compatible
   var mmm =
     ( 1==m)?'Jan':( 2==m)?'Feb':(3==m)?'Mar':
     ( 4==m)?'Apr':( 5==m)?'May':(6==m)?'Jun':
@@ -27,24 +21,17 @@ function date_ddmmmyy(date)
     (10==m)?'Oct':(11==m)?'Nov':'Dec';
 
   return "" +
-    (d<10?"0"+d:d) + "-" +
-    mmm + "-" +
-    (y<10?"0"+y:y);
+    (d<10?"0"+d:d) + " " +
+    mmm + " " +
+    (y<10?"0"+y:Y);
 }
 
-
-//
-// get last modified date of the
-// current document.
-//
 function date_lastmodified()
 {
   var lmd = document.lastModified;
   var s   = "Unknown";
   var d1;
 
-  // check if we have a valid date
-  // before proceeding
   if(0 != (d1=Date.parse(lmd)))
   {
     s = "" + date_ddmmmyy(new Date(d1));
@@ -53,12 +40,6 @@ function date_lastmodified()
   return s;
 }
 
-//
-// finally display the last modified date
-// as DD-MMM-YY
-//
 document.write(
-  "This page was updated on " +
+  "Updated on " +
   date_lastmodified() );
-
-// -->
