1d4fb4c6f8681a78ec97e55d9256beee4faca2f8
steffenwoell <56505661+steffenwoell@users.noreply.github.com>
2021-05-29 09:01:26 +0200

    Update lastmod.js

--- js/lastmod.js
+++ js/lastmod.js
@@ -1,33 +1,8 @@
-<!--
-function showFileModified() {
-        var input, file;
-
-        // Testing for 'function' is more specific and correct, but doesn't work with Safari 6.x
-        if (typeof window.FileReader !== 'function' &&
-            typeof window.FileReader !== 'object') {
-            write("The file API isn't supported on this browser yet.");
-            return;
-        }
-
-        input = document.getElementById('filename');
-        if (!input) {
-            write("Um, couldn't find the filename element.");
-        }
-        else if (!input.files) {
-            write("This browser doesn't seem to support the `files` property of file inputs.");
-        }
-        else if (!input.files[0]) {
-            write("Please select a file before clicking 'Show Modified'");
-        }
-        else {
-            file = input.files[0];
-            write("The last modified date of file '" + file.name + "' is " + new Date(file.lastModified));
-        }
-
-        function write(msg) {
-            var p = document.createElement('p');
-            p.innerHTML = msg;
-            document.body.appendChild(p);
-        }
+function get()
+    {
+        var myObject, f, date;
+        myObject = new ActiveXObject("Scripting.FileSystemObject");
+        f = myObject.GetFile("c:\\test.txt");
+        date = f.DateLastModified;
+        alert ("The date this file was last modified is: " + date);
     }
-//-->
