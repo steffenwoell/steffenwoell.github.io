c9939af104580f747a781861314e1f12adec6826
Steffen <steffen.woell@gmail.com>
2023-12-17 17:56:35 +0100

    css exp

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var main = {
   numImgs : null,
 
   init : function() {
-    // Shorten the navbar after scrolling a little bit down
+    // Shorten navbar after scrolling a little down
     $(window).scroll(function() {
         if ($(".navbar").offset().top > 50) {
             $(".navbar").addClass("top-nav-short");
@@ -17,7 +17,7 @@ var main = {
         }
     });
 
-    // On mobile, hide the avatar when expanding the navbar menu
+    // On mobile, hide avatar when expanding navbar menu
     $('#main-navbar').on('show.bs.collapse', function () {
       $(".navbar").addClass("top-nav-expanded");
     });
@@ -25,7 +25,7 @@ var main = {
       $(".navbar").removeClass("top-nav-expanded");
     });
 
-    // On mobile, when clicking on a multi-level navbar menu, show the child links
+    // On mobile, when clicking on multi-level navbar menu, show child links
     $('#main-navbar').on("click", ".navlinks-parent", function(e) {
       var target = e.target;
       $.each($(".navlinks-parent"), function(key, value) {
@@ -37,7 +37,7 @@ var main = {
       });
     });
 
-    // Ensure nested navbar menus are not longer than the menu header
+    // Ensure nested navbar menus are not longer than menu header
     var menus = $(".navlinks-container");
     if (menus.length > 0) {
       var navbar = $("#main-navbar ul");
@@ -64,24 +64,23 @@ var main = {
       fakeMenu.remove();
     }
 
-    // show the big header image
+    // Show big header image
     main.initImgs();
   },
 
   initImgs : function() {
-    // If the page was large images to randomly select from, choose an image
+    // If page has large images to randomly select from, choose one
     if ($("#header-big-imgs").length > 0) {
       main.bigImgEl = $("#header-big-imgs");
       main.numImgs = main.bigImgEl.attr("data-num-img");
 
-          // 2fc73a3a967e97599c9763d05e564189
-	  // set an initial image
+	  // set initial image
 	  var imgInfo = main.getImgInfo();
 	  var src = imgInfo.src;
 	  var desc = imgInfo.desc;
   	  main.setImg(src, desc);
 
-	  // For better UX, prefetch the next image so that it will already be loaded when we want to show it
+	  // For better UX, prefetch next image
   	  var getNextImg = function() {
 	    var imgInfo = main.getImgInfo();
 	    var src = imgInfo.src;
@@ -89,25 +88,20 @@ var main = {
 
 		var prefetchImg = new Image();
   		prefetchImg.src = src;
-		// if I want to do something once the image is ready: `prefetchImg.onload = function(){}`
 
   		setTimeout(function(){
                   var img = $("<div></div>").addClass("big-img-transition").css("background-image", 'url(' + src + ')');
   		  $(".intro-header.big-img").prepend(img);
   		  setTimeout(function(){ img.css("opacity", "1"); }, 50);
-
-		  // after the animation of fading in the new image is done, prefetch the next one
-  		  //img.one("transitioned webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
 		  setTimeout(function() {
 		    main.setImg(src, desc);
 			img.remove();
   			getNextImg();
 		  }, 1000);
-  		  //});
   		}, 6000);
   	  };
 
-	  // If there are multiple images, cycle through them
+	  // If multiple images, cycle through
 	  if (main.numImgs > 1) {
   	    getNextImg();
 	  }
@@ -134,7 +128,5 @@ var main = {
 	}
   }
 };
-
-// 2fc73a3a967e97599c9763d05e564189
 
 document.addEventListener('DOMContentLoaded', main.init);
